1f24fe293fcacf6be7d21d70f4e659088c48336f
harshitabhaskaruni <harshitabhaskaruni@gmail.com>
2025-11-11 18:18:51 +0000

    Updated Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Welcome from "./pages/Welcome";
+import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/Signup";
 import Login from "./pages/Login";
 import Explore from "./pages/Explore";
@@ -14,7 +14,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/explore" element={<Explore />} />
