3963d269382566d184e93e527fc5e6eeddfe2888
Kishlaya Mishra <kishlayamishra@gmail.com>
2026-02-08 22:10:58 +0530

    on explore page, logo looks too big, fixed that 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -99,7 +99,7 @@ const Navbar = () => {
         <div className="flex h-16 items-center justify-between gap-4">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2 group shrink-0">
-            <YavuliLogoAnimation className={isActive('/explore') ? "max-w-[140px]" : "max-w-[200px]"} />
+            <YavuliLogoAnimation className={location.pathname === "/" ? "max-w-[200px]" : "max-w-[140px]"} />
           </Link>
 
           {/* Search Bar - Desktop */}
